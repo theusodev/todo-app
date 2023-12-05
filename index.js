5eb8e8a5a248dd55f1e3d5da17f442dc0c7f5483
Matheus Oliveira <matheuspiudeoliveira@gmail.com>
2023-12-05 16:02:34 -0300

    exibição das tarefas na página de home concluída

--- index.js
+++ index.js
@@ -50,9 +50,10 @@ app.get('/', (requisicao, resposta) => {
                 completa: dado.completa === 0 ? false : true
             }
         })
+
+        resposta.render('home', { tarefas })
     })
     
-    resposta.render('home')
 })
 
 const conexao = mysql.createConnection({
