473cca2e3b6df1e43b42be2195e00ca2d0eb93e5
Matheus Oliveira <matheuspiudeoliveira@gmail.com>
2023-12-05 16:12:33 -0300

    filtrando as tarefas ativas

--- index.js
+++ index.js
@@ -35,6 +35,24 @@ app.post('/completar', (requisicao, resposta) => {
     })
 })
 
+app.post('/descompletar', (requisicao, resposta) => {
+    const id = requisicao.body.id
+
+    const sql = `
+        UPDATE tarefas
+        SET completa = '0'
+        WHERE id = ${id}
+    `
+
+    conexao.query(sql, (erro) =>{
+        if (erro) {
+            return console.log(erro)
+        }
+
+        resposta.redirect('/')
+    })
+})
+
 app.post('/criar', (requisicao, resposta) => {
     const descricao = requisicao.body.descricao
     const completa = 0
@@ -69,7 +87,13 @@ app.get('/', (requisicao, resposta) => {
             }
         })
 
-        resposta.render('home', { tarefas })
+        const tarefasAtivas = tarefas.filter((tarefa) =>{
+            return tarefa.completa === false && tarefa
+        })
+
+        const quantidadeTarefasAtivas = tarefasAtivas.length
+
+        resposta.render('home', { tarefas, quantidadeTarefasAtivas });
     })
     
 })
